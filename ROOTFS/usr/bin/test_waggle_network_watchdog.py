dd515de56fa8b6c14ed17be10180b97a4dcd3834
Sean Shahkarami <sean.shahkarami@gmail.com>
2021-03-30 15:26:44 -0500

    run fast simulation of watchdog actions. mocking out reboots, shutdowns, nv commands, etc.

--- ROOTFS/usr/bin/test_waggle_network_watchdog.py
+++ ROOTFS/usr/bin/test_waggle_network_watchdog.py
@@ -1,132 +1,43 @@
 #!/usr/bin/env python3
 import logging
-import unittest
-from waggle_network_watchdog import Watchdog
-
-
-class TestWatchdog(unittest.TestCase):
-
-    def test_healthy(self):
-        watchdog = Watchdog(
-            time_func=lambda: 0,
-            health_check=lambda: True,
-            health_check_passed=lambda timer: None,
-            health_check_failed=lambda timer: None,
-            recovery_actions=[],
-        )
-        watchdog.update()
-
-    def test_simple(self):
-        network_good = True
-
-        def health_check():
-            return network_good
-
-        def health_check_passed(timer):
-            pass
-
-        def health_check_failed(timer):
-            pass
-
-        called = [0, 0]
-
-        def action1():
-            called[0] += 1
-
-        def action2():
-            called[1] += 1
-
-        recovery_actions = [
-            (50, action1),
-            (100, action1),
-            (200, action2),
-        ]
-
-        watchdog = Watchdog(
-            health_check=health_check,
-            health_check_passed=health_check_passed,
-            health_check_failed=health_check_failed,
-            recovery_actions=recovery_actions,
-        )
-
-        # simulate failing network
-        network_good = False
-        for _ in range(20):
-            watchdog.tick(15)
-
-        # restore network briefly
-        network_good = True
-        for _ in range(2):
-            watchdog.tick(15)
-
-        # simulate failing network again
-        network_good = False
-        for _ in range(20):
-            watchdog.tick(15)
-
-        self.assertTrue(all(c > 0 for c in called))
+import sys
 
 
 def main():
+    import waggle_network_watchdog
+
     logging.basicConfig(level=logging.INFO)
 
-    network_good = True
-
-    def health_check():
-        return network_good
-
-    def health_check_passed(timer):
-        logging.info("connection ok")
-
-    def health_check_failed(timer):
-        logging.warning("connection failed at %ss seconds", timer)
-
-    def action1():
-        logging.info("action 1")
-
-    def action2():
-        logging.info("action 2")
-
-    def action3():
-        logging.info("action 3")
-
-    recovery_actions = [
-        (55, action1),
-        (123, action2),
-        (234, action3),
-    ]
-
-    current_time = 123
+    current_time = 0.0
 
     def fake_time():
         return current_time
 
-    watchdog = Watchdog(
+    def fake_reboot_os():
+        logging.warning("ACTION reboot os")
+        sys.exit(0)
+
+    def fake_shutdown_os():
+        logging.warning("ACTION shutdown os")
+        sys.exit(0)
+
+    waggle_network_watchdog.reboot_os = fake_reboot_os
+    waggle_network_watchdog.shutdown_os = fake_shutdown_os
+
+    real_watchdog = waggle_network_watchdog.build_watchdog()
+
+    watchdog = waggle_network_watchdog.Watchdog(
         time_func=fake_time,
-        health_check=health_check,
-        health_check_passed=health_check_passed,
-        health_check_failed=health_check_failed,
-        recovery_actions=recovery_actions,
+        health_check=lambda: False,
+        health_check_passed=real_watchdog.health_check_passed,
+        health_check_failed=real_watchdog.health_check_failed,
+        recovery_actions=real_watchdog.recovery_actions,
     )
 
-    # simulate failing network
-    network_good = False
-    for _ in range(20):
-        current_time += 15
+    for _ in range(10000):
+        current_time += 15.0
         watchdog.update()
 
-    # restore network briefly
-    network_good = True
-    for _ in range(2):
-        current_time += 15
-        watchdog.update()
-
-    # simulate failing network again
-    network_good = False
-    for _ in range(20):
-        current_time += 15
-        watchdog.update()
 
 if __name__ == "__main__":
-    # unittest.main()
     main()
