e416233542c6096da37af3cf97ff0398b5d1a046
Joe Swantek <joseph.swantek@northwestern.edu>
2021-12-02 16:36:40 -0600

    SAGE-1127: auto-linting

--- ROOTFS/usr/bin/waggle_network_watchdog.py
+++ ROOTFS/usr/bin/waggle_network_watchdog.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
-from pathlib import Path
-import subprocess
-import time
+import ast
+import configparser
+import json
 import logging
 import socket
+import subprocess
+import time
 from glob import glob
-import configparser
-from typing import NamedTuple, Callable
-import json
-import ast
+from pathlib import Path
+from typing import Callable, NamedTuple
 
 MEDIA_MMC = 0
 MEDIA_SD = 1
+
 
 class Action(NamedTuple):
     thresh: int
@@ -19,8 +20,9 @@ class Action(NamedTuple):
 
 
 class Watchdog:
-
-    def __init__(self, time_func, recovery_actions, health_check, health_check_passed, health_check_failed):
+    def __init__(
+        self, time_func, recovery_actions, health_check, health_check_passed, health_check_failed
+    ):
         self.time_func = time_func
         self.recovery_actions = [Action(thresh, func) for thresh, func in recovery_actions]
         # important: tick function expects recovery actions to sorted by thresh in increasing order
@@ -59,6 +61,7 @@ class Watchdog:
 class WatchdogConfig(NamedTuple):
     nwwd_ok_file: str
 
+
 class NetworkWatchdogConfig(NamedTuple):
     check_seconds: float
     check_successive_passes: int
@@ -75,7 +78,7 @@ class NetworkWatchdogConfig(NamedTuple):
     hard_resets: list
     hard_num_resets: int
     hard_reset_file: str
-    
+
 
 class ReverseTunnelConfig(NamedTuple):
     beekeeper_server: str
@@ -96,6 +99,7 @@ def read_config_section_dict(filename, section):
 
     return {}
 
+
 def read_watchdog_config(filename, section="watchdog"):
     d = read_config_section_dict(filename, section)
 
@@ -103,36 +107,36 @@ def read_watchdog_config(filename, section="watchdog"):
         nwwd_ok_file=d.get("ssh_ok_file", None),
     )
 
+
 def read_network_watchdog_config(filename):
     all_settings = read_config_section_dict(filename, "all")
     network_reset_settings = read_config_section_dict(filename, "network-reboot")
     soft_reset_settings = read_config_section_dict(filename, "soft-reboot")
     hard_reset_settings = read_config_section_dict(filename, "hard-reboot")
 
-    sd_card_storage_loc = ''
+    sd_card_storage_loc = ""
     if read_current_media() == MEDIA_SD:
         sd_card_storage_loc = all_settings.get("sd_card_storage_loc", None)
-    
+
     return NetworkWatchdogConfig(
         current_media=read_current_media(),
         network_resets=json.loads(network_reset_settings.get("resets", None)),
         network_num_resets=int(network_reset_settings.get("num_resets", 0)),
-        network_reset_file=sd_card_storage_loc+network_reset_settings.get("current_reset_file", None),
-        
-	soft_resets=json.loads(soft_reset_settings.get("resets", None)),
+        network_reset_file=sd_card_storage_loc
+        + network_reset_settings.get("current_reset_file", None),
+        soft_resets=json.loads(soft_reset_settings.get("resets", None)),
         soft_num_resets=int(soft_reset_settings.get("num_resets", 0)),
-        soft_reset_file=sd_card_storage_loc+soft_reset_settings.get("current_reset_file", None),
-	
-	hard_resets=json.loads(hard_reset_settings.get("resets", None)),
+        soft_reset_file=sd_card_storage_loc + soft_reset_settings.get("current_reset_file", None),
+        hard_resets=json.loads(hard_reset_settings.get("resets", None)),
         hard_num_resets=int(hard_reset_settings.get("num_resets", 0)),
-        hard_reset_file=sd_card_storage_loc+hard_reset_settings.get("current_reset_file", None),
-        
-        rssh_addrs=list(ast.literal_eval(all_settings.get("rssh_addrs",None))),
-        network_services=json.loads(all_settings.get("network_services",None)),
-	check_seconds=float(all_settings.get("check_seconds", 15.0)),
+        hard_reset_file=sd_card_storage_loc + hard_reset_settings.get("current_reset_file", None),
+        rssh_addrs=list(ast.literal_eval(all_settings.get("rssh_addrs", None))),
+        network_services=json.loads(all_settings.get("network_services", None)),
+        check_seconds=float(all_settings.get("check_seconds", 15.0)),
         check_successive_passes=int(all_settings.get("check_successive_passes", 3)),
         check_successive_seconds=float(all_settings.get("check_successive_seconds", 5.0)),
     )
+
 
 def read_reverse_tunnel_config(filename, section="reverse-tunnel"):
     d = read_config_section_dict(filename, section)
@@ -157,16 +161,12 @@ def ssh_connection_ok(server, port):
         server_addr = f"{socket.gethostbyname(server)}:{port}"
         logging.debug(f"checking for ssh connection to [{server_addr}]")
 
-        return (
-            server_addr
-            in subprocess.check_output(["ss", "-t", "state", "established"]).decode()
-        )
+        return server_addr in subprocess.check_output(["ss", "-t", "state", "established"]).decode()
     except Exception:
         return False
 
-def require_successive_passes(
-    check_func, server, port, successive_passes, successive_seconds
-):
+
+def require_successive_passes(check_func, server, port, successive_passes, successive_seconds):
     for _ in range(successive_passes):
         if not check_func(server, port):
             return False
@@ -191,12 +191,14 @@ def restart_network_services(nwwd_config):
     fix_modem_port_settings()
 
     # restart network services
-    subprocess.run( ['systemctl', 'restart'] + nwwd_config.network_services )
+    subprocess.run(["systemctl", "restart"] + nwwd_config.network_services)
+
 
 def reboot_os():
     logging.warning("rebooting the system")
     # aggressively but safely reboot the system
     subprocess.run(["systemctl", "--force", "reboot"])
+
 
 def shutdown_os():
     logging.warning("shutting down the system")
@@ -234,12 +236,12 @@ def build_rec_actions(nwwd_config):
                 subprocess.run(["nvbootctrl", "set-active-boot-slot", "1"])
             else:
                 subprocess.run(["nvbootctrl", "set-active-boot-slot", "0"])
-	    
+
             reboot_os()
 
     # Recovery actions table [time (s), recovery function]
     # restart networking stack after 15, 20 and 25 of no beehive connectivity
-    # reboot after 30 mins of no beehive connectivity shutdown after 1 hour 
+    # reboot after 30 mins of no beehive connectivity shutdown after 1 hour
     # of no connection to beehive after 3 30 minute reboots
     #
     # NOTE We sort in increasing order of threshold so that our linear
@@ -260,18 +262,20 @@ def build_rec_actions(nwwd_config):
 
 def read_resets_safe(reset_file):
     try:
-        with open(reset_file, 'r') as f:
+        with open(reset_file, "r") as f:
             return int(f.readline())
     except Exception:
         logging.warning("Unable to read from file: %s", reset_file)
         return 0
 
+
 def write_resets_safe(reset_file, resets):
     try:
-        with open(reset_file, 'w') as f:
-            f.write('%d' % resets)
+        with open(reset_file, "w") as f:
+            f.write("%d" % resets)
     except Exception:
         logging.warning("Unable to write to file: %s", reset_file)
+
 
 def read_current_resets(reset_file):
     resets = 0
@@ -281,7 +285,7 @@ def read_current_resets(reset_file):
 
         Path(folder).mkdir(parents=True, exist_ok=True)
         Path(reset_file).touch()
-        
+
         write_resets_safe(reset_file, resets)
     else:
         return read_resets_safe(reset_file)
@@ -292,9 +296,10 @@ def read_current_resets(reset_file):
 def write_current_resets(reset_file, current_resets):
     write_resets_safe(reset_file, current_resets)
 
+
 def increment_reset_file(reset_file):
     resets = read_current_resets(reset_file)
-    write_current_resets(reset_file, resets+1)
+    write_current_resets(reset_file, resets + 1)
 
 
 def update_reset_file(reset_file, value):
@@ -302,37 +307,48 @@ def update_reset_file(reset_file, value):
     if value != file_value:
         write_current_resets(reset_file, value)
 
+
 def read_current_media():
-        return 1 if '1' in subprocess.check_output(["nvbootctrl", "get-current-slot"]).decode() else 0
+    return 1 if "1" in subprocess.check_output(["nvbootctrl", "get-current-slot"]).decode() else 0
 
 
 def build_watchdog():
     nwwd_config = read_network_watchdog_config("/etc/waggle/nw/config.ini")
     rssh_config = read_reverse_tunnel_config("/etc/waggle/config.ini")
-    
+
     def health_check():
         logging.info("checking connections to any of %s", nwwd_config.rssh_addrs)
-    
+
         health = False
         for alias, server, port in nwwd_config.rssh_addrs:
-            curServerHealth = require_successive_passes(ssh_connection_ok,
-                                                        server,
-                                                        port,
-                                                        nwwd_config.check_successive_passes,
-                                                        nwwd_config.check_successive_seconds) 
+            curServerHealth = require_successive_passes(
+                ssh_connection_ok,
+                server,
+                port,
+                nwwd_config.check_successive_passes,
+                nwwd_config.check_successive_seconds,
+            )
 
             health = health or curServerHealth
             logging.debug(f"Reporting ssh connection of {alias} as {curServerHealth}")
-            
+
             try:
-                subprocess.check_call(["waggle-publish-metric", "sys.rssh_up", str(int(curServerHealth)), "--meta", "server=" + alias])
+                subprocess.check_call(
+                    [
+                        "waggle-publish-metric",
+                        "sys.rssh_up",
+                        str(int(curServerHealth)),
+                        "--meta",
+                        "server=" + alias,
+                    ]
+                )
             except Exception:
                 logging.warning("waggle-publish-metric not found. no metrics will be published")
-    
+
         return health
 
     def health_check_passed(timer):
-        logging.info("connection ok")    
+        logging.info("connection ok")
         update_reset_file(nwwd_config.hard_reset_file, 0)
         update_reset_file(nwwd_config.soft_reset_file, 0)
         update_reset_file(nwwd_config.network_reset_file, 0)
@@ -350,19 +366,20 @@ def build_watchdog():
         recovery_actions=recovery_actions,
     )
 
+
 def main():
-    subprocess.run(['nvbootctrl', 'dump-slots-info'])
+    subprocess.run(["nvbootctrl", "dump-slots-info"])
     subprocess.run(["nv_update_engine", "-v"])
-    subprocess.run(['nvbootctrl', 'mark-boot-successful'])
+    subprocess.run(["nvbootctrl", "mark-boot-successful"])
 
     logging.basicConfig(level=logging.INFO)
     logging.info("marked boot as successful for media %s", read_current_media())
     logging.info("Slots info after marking boot successful:")
-    subprocess.run(['nvbootctrl', 'dump-slots-info'])
+    subprocess.run(["nvbootctrl", "dump-slots-info"])
 
     nwwd_config = read_network_watchdog_config("/etc/waggle/nw/config.ini")
     wd_config = read_watchdog_config("/etc/waggle/config.ini")
-    
+
     watchdog = build_watchdog()
 
     while True:
@@ -374,8 +391,9 @@ def main():
         # update hardware watchdog
         if wd_config.nwwd_ok_file is not None:
             Path(wd_config.nwwd_ok_file).touch()
-        
+
         time.sleep(nwwd_config.check_seconds)
+
 
 if __name__ == "__main__":
     main()
