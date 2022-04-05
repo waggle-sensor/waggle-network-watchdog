220d1bc5b691032fa4043b15a44160ae472c9c29
Joe Swantek <joseph.swantek@northwestern.edu>
2022-04-05 17:42:46 -0500

    SAGE-1292: convert to use deb-builder

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM ubuntu:18.04
-
-RUN apt-get update -y && apt-get install -y 
-
-RUN mkdir -p /deb/ /ROOTFS/
-ADD deb /deb/
-ADD ROOTFS /ROOTFS/
-
-COPY release.sh .
--- build.sh
+++ build.sh
@@ -1,39 +1,8 @@
 #!/bin/bash -e
 
-print_help() {
-  echo """
-usage: build.sh [-f]
-Create the versioned Debian package.
- -f : force the build to proceed (debugging only) without checking for tagged commit
-"""
-}
-
-FORCE=
-while getopts "f?" opt; do
-  case $opt in
-    f) # force build
-      echo "** Force build: ignore tag depth check **"
-      FORCE=1
-      ;;
-    ?|*)
-      print_help
-      exit 1
-      ;;
-  esac
-done
-
-# determine full version
-VERSION_SHORT=$(git describe --tags --dirty | cut -c2-)
-VERSION_LONG=$(git describe --tags --long --dirty | cut -c2-)
-
-TAG_DEPTH=$(echo ${VERSION_LONG} | cut -d '-' -f 2)
-if [[ -z "${FORCE}" && "${TAG_DEPTH}_" != "0_" ]]; then
-  echo "Error:"
-  echo "  The current git commit has not been tagged. Please create a new tag first to ensure a proper unique version number."
-  echo "  Use -f to ignore error (for debugging only)."
-  exit 1
-fi
-
-docker build -t network_watchdog .
-docker run --rm --privileged \
--v `pwd`:/output/ -e VERSION_SHORT=$VERSION_SHORT -e VERSION_LONG=$VERSION_LONG network_watchdog ./release.sh 
+docker run --rm \
+  -e NAME="waggle-network-watchdog" \
+  -e DESCRIPTION="NX Network Watchdog Services" \
+  -e "MAINTAINER=sagecontinuum.org" \
+  -v "$PWD:/repo" \
+  waggle/waggle-deb-builder:0.2.0
